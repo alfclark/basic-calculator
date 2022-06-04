ff3f08cb51f8761682a7241553b3339662e60060
AlfClark <alfredoclarkcardenas@gmail.com>
2022-06-04 12:11:23 -0700

    Functions clear and delete

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,19 @@ const equal = document.getElementById("equal");
 
 const result = document.getElementById("result");
 const operation = document.getElementById("operation");
+
+clearBtn.addEventListener("click", clearScreen);
+deleteBtn.addEventListener("click", deleteNum);
+
+function clearScreen() {
+  result.textContent = 0;
+  operation.style.opacity = 0;
+}
+
+function deleteNum() {
+  if (result.textContent.length >= 2) {
+    result.textContent = result.textContent.toString().slice(0, -1);
+  } else if (result.textContent.length < 2) {
+    result.textContent = 0;
+  }
+}
