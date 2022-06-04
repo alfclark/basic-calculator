733886ae2f34d607b562d948ab01340d6178b6cf
AlfClark <alfredoclarkcardenas@gmail.com>
2022-06-04 12:04:46 -0700

    DOM Elements binding

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+const numberBtns = document.querySelectorAll("[data-number]");
+const operatorBtns = document.querySelectorAll("[data-operator]");
+const clearBtn = document.getElementById("clearBtn");
+const deleteBtn = document.getElementById("deleteBtn");
+const dotBtn = document.getElementById("dot");
+const equal = document.getElementById("equal");
+
+const result = document.getElementById("result");
+const operation = document.getElementById("operation");
